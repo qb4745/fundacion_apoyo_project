0666aec9a61a4be3cfbe14765a15d93a6a5e66ad
Jaime Vicencio <jai.vicencio@duocuc.cl>
2023-07-11 08:44:48 -0400

    roles funcionando

--- users/mixins.py
+++ users/mixins.py
@@ -1,8 +1,19 @@
 from django.contrib.auth.mixins import AccessMixin
 from django.core.exceptions import PermissionDenied
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 class StaffRequiredMixin(AccessMixin):
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated or not request.user.is_staff:
             raise PermissionDenied
         return super().dispatch(request, *args, **kwargs)
+
+
+class AdministrativoRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.groups.filter(name='Administrativo').exists()
+
+
+class EnfermeraRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.groups.filter(name='Enfermera/o').exists()
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render, reverse
 from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView, ListView, DetailView
@@ -9,7 +10,7 @@ from .forms import (MandatoForm, ResidenteForm, ResidenteDetailForm,
                     DosisMedicamentoForm, PlanMedicacionDetailForm, FichaMedicaForm,
                     FichaMedicaDetailForm
                     )
-from .mixins import StaffRequiredMixin
+from .mixins import StaffRequiredMixin, AdministrativoRequiredMixin, EnfermeraRequiredMixin
 
 
 class DashBoardView(LoginRequiredMixin, TemplateView):
@@ -67,41 +68,68 @@ class MandatoDeleteView(LoginRequiredMixin, DeleteView):
         return reverse("users:users-mandato")
 
 
-class ResidenteListView(LoginRequiredMixin, StaffRequiredMixin, ListView):
+class ResidenteListView(LoginRequiredMixin, StaffRequiredMixin, PermissionRequiredMixin, ListView):
     model = Residente
     template_name = 'users/residente_list.html'
     context_object_name = 'residentes'
+    permission_required = 'users.view_residente'
 
     def get_queryset(self):
         queryset = super().get_queryset()
         queryset = queryset.order_by('-estado_en_hogar')  # Sort by estado_en_hogar field in descending order
         return queryset
 
+    def handle_no_permission(self):
+        try:
+            return super().handle_no_permission()
+        except PermissionDenied:
+            return render(self.request, 'users/permission_denied.html', status=403)
 
-class ResidenteCreateView(LoginRequiredMixin, StaffRequiredMixin, CreateView):
+
+class ResidenteCreateView(LoginRequiredMixin, AdministrativoRequiredMixin, PermissionRequiredMixin, CreateView):
     template_name = 'users/residente_create.html'
     form_class = ResidenteForm
+    permission_required = 'users.create_residente'
 
 
     def get_success_url(self):
         return reverse("users:users-residente-list")
 
+    def handle_no_permission(self):
+        try:
+            return super().handle_no_permission()
+        except PermissionDenied:
+            return render(self.request, 'users/permission_denied.html', status=403)
 
-class ResidenteUpdateView(LoginRequiredMixin, StaffRequiredMixin, UpdateView):
+
+class ResidenteUpdateView(LoginRequiredMixin, AdministrativoRequiredMixin, PermissionRequiredMixin, UpdateView):
     template_name = 'users/residente_update.html'
     form_class = ResidenteForm
     queryset = Residente.objects.all()
+    permission_required = 'users.create_residente'
 
     def get_success_url(self):
         return reverse("users:users-residente-list")
 
+    def handle_no_permission(self):
+        try:
+            return super().handle_no_permission()
+        except PermissionDenied:
+            return render(self.request, 'users/permission_denied.html', status=403)
 
-class ResidenteDeleteView(LoginRequiredMixin, StaffRequiredMixin, DeleteView):
+class ResidenteDeleteView(LoginRequiredMixin, AdministrativoRequiredMixin, PermissionRequiredMixin, DeleteView):
     template_name = 'users/residente_delete.html'
     queryset = Residente.objects.all()
+    permission_required = 'users.create_residente'
 
     def get_success_url(self):
         return reverse("users:users-residente-list")
+
+    def handle_no_permission(self):
+        try:
+            return super().handle_no_permission()
+        except PermissionDenied:
+            return render(self.request, 'users/permission_denied.html', status=403)
 
 class ResidenteDetailView(LoginRequiredMixin, StaffRequiredMixin, DetailView):
     template_name = 'users/residente_detail.html'
@@ -130,13 +158,20 @@ class ResidenteIngresoEgresoListView(LoginRequiredMixin, StaffRequiredMixin, Lis
         return queryset
 
 
-class ResidenteIngresoEgresoUpdateView(LoginRequiredMixin, StaffRequiredMixin, UpdateView):
+class ResidenteIngresoEgresoUpdateView(LoginRequiredMixin,  AdministrativoRequiredMixin, PermissionRequiredMixin, UpdateView):
     template_name = 'users/residente_ingreso_egreso_update.html'
     form_class = ResidentIngresoEgresoForm
     queryset = Residente.objects.all()
+    permission_required = 'users.update_residente'
 
     def get_success_url(self):
         return reverse("users:users-residente-ingreso-egreso-list")
+
+    def handle_no_permission(self):
+        try:
+            return super().handle_no_permission()
+        except PermissionDenied:
+            return render(self.request, 'users/permission_denied.html', status=403)
 
 
 from .models import Medicamento, PlanMedicacion, DosisMedicamento
@@ -154,12 +189,14 @@ class MedicamentoListView(LoginRequiredMixin, StaffRequiredMixin, ListView):
         return queryset
 
 
+
 class MedicamentoCreateView(LoginRequiredMixin, StaffRequiredMixin, CreateView):
     template_name = 'users/medicamento_create.html'
     form_class = MedicamentoForm
 
     def get_success_url(self):
         return reverse("users:users-medicamento-list")
+
 
 
 class MedicamentoUpdateView(LoginRequiredMixin, StaffRequiredMixin, UpdateView):
@@ -190,26 +227,47 @@ class PlanMedicacionListView(LoginRequiredMixin, StaffRequiredMixin, ListView):
         return queryset
 
 
-class PlanMedicacionCreateView(LoginRequiredMixin, StaffRequiredMixin, CreateView):
+class PlanMedicacionCreateView(LoginRequiredMixin, EnfermeraRequiredMixin, PermissionRequiredMixin, CreateView):
     template_name = 'users/planmedicacion_create.html'
     form_class = PlanMedicacionForm
     success_url = reverse_lazy('users:users-planmedicacion-list')
+    permission_required = 'users.add_planmedicacion'
+
+    def handle_no_permission(self):
+        try:
+            return super().handle_no_permission()
+        except PermissionDenied:
+            return render(self.request, 'users/permission_denied.html', status=403)
 
 
 
-class PlanMedicacionUpdateView(LoginRequiredMixin, StaffRequiredMixin, UpdateView):
+class PlanMedicacionUpdateView(LoginRequiredMixin, EnfermeraRequiredMixin, PermissionRequiredMixin, UpdateView):
     template_name = 'users/planmedicacion_update.html'
     form_class = PlanMedicacionForm
     queryset = PlanMedicacion.objects.all()
+    permission_required = 'users.change_planmedicacion'
 
     def get_success_url(self):
         return reverse("users:users-planmedicacion-list")
 
+    def handle_no_permission(self):
+        try:
+            return super().handle_no_permission()
+        except PermissionDenied:
+            return render(self.request, 'users/permission_denied.html', status=403)
 
-class PlanMedicacionDeleteView(LoginRequiredMixin, StaffRequiredMixin, DeleteView):
+
+class PlanMedicacionDeleteView(LoginRequiredMixin, EnfermeraRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = PlanMedicacion
     success_url = reverse_lazy('users:users-planmedicacion-list')
     # Puedes especificar el template_name si deseas utilizar un template personalizado
+    permission_required = 'users.delete_planmedicacion'
+
+    def handle_no_permission(self):
+        try:
+            return super().handle_no_permission()
+        except PermissionDenied:
+            return render(self.request, 'users/permission_denied.html', status=403)
 
 
 
@@ -227,31 +285,53 @@ class DosisMedicamentoListView(LoginRequiredMixin, StaffRequiredMixin, ListView)
         return queryset
 
 
-class DosisMedicamentoCreateView(LoginRequiredMixin, StaffRequiredMixin, CreateView):
+class DosisMedicamentoCreateView(LoginRequiredMixin, EnfermeraRequiredMixin, PermissionRequiredMixin, CreateView):
     template_name = 'users/dosismedicamentos_create.html'
     form_class = DosisMedicamentoForm
     success_url = reverse_lazy('users:users-dosismedicamento-list')
+    permission_required = 'users.add_dosismedicamento'
 
-class DosisMedicamentoUpdateView(LoginRequiredMixin, StaffRequiredMixin, UpdateView):
+    def handle_no_permission(self):
+        try:
+            return super().handle_no_permission()
+        except PermissionDenied:
+            return render(self.request, 'users/permission_denied.html', status=403)
+
+
+class DosisMedicamentoUpdateView(LoginRequiredMixin, EnfermeraRequiredMixin, PermissionRequiredMixin, UpdateView):
     template_name = 'users/dosismedicamentos_update.html'
     form_class = DosisMedicamentoForm
     queryset = DosisMedicamento.objects.all()
+    permission_required = 'users.change_dosismedicamento'
 
     def get_success_url(self):
         dosis_medicamento = self.get_object()
         plan_medicacion_pk = dosis_medicamento.plan_medicacion.pk
         return reverse('users:users-planmedicacion-detail', kwargs={'pk': plan_medicacion_pk})
 
+    def handle_no_permission(self):
+        try:
+            return super().handle_no_permission()
+        except PermissionDenied:
+            return render(self.request, 'users/permission_denied.html', status=403)
 
-class DosisMedicamentoDeleteView(LoginRequiredMixin, StaffRequiredMixin, DeleteView):
+
+class DosisMedicamentoDeleteView(LoginRequiredMixin, EnfermeraRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = DosisMedicamento
     success_url = reverse_lazy('users:users-dosismedicamento-list')
+    permission_required = 'users.delete_dosismedicamento'
     # Puedes especificar el template_name si deseas utilizar un template personalizado
 
     def get_success_url(self):
         dosis_medicamento = self.get_object()
         plan_medicacion_pk = dosis_medicamento.plan_medicacion.pk
         return reverse('users:users-planmedicacion-detail', kwargs={'pk': plan_medicacion_pk})
+
+    def handle_no_permission(self):
+        try:
+            return super().handle_no_permission()
+        except PermissionDenied:
+            return render(self.request, 'users/permission_denied.html', status=403)
 
 
 class PlanMedicacionDetailView(LoginRequiredMixin, StaffRequiredMixin, DetailView):
@@ -267,10 +347,11 @@ class PlanMedicacionDetailView(LoginRequiredMixin, StaffRequiredMixin, DetailVie
         return context
 
 
-class DosisMedicamentoResidenteCreateView(LoginRequiredMixin, StaffRequiredMixin, CreateView):
+class DosisMedicamentoResidenteCreateView(LoginRequiredMixin, EnfermeraRequiredMixin, PermissionRequiredMixin, CreateView):
     model = DosisMedicamento
     fields = ['medicamento', 'dosis_diarias', 'hora_administracion']
     template_name = 'users/dosismedicamentos_create.html'
+    permission_required = 'users.add_dosismedicamento'
 
     def form_valid(self, form):
         plan_medicacion = get_object_or_404(PlanMedicacion, pk=self.kwargs['pk'])
@@ -295,46 +376,54 @@ class FichaMedicaListView(LoginRequiredMixin, StaffRequiredMixin, ListView):
         return queryset
 
 
-class FichaMedicaCreateView(LoginRequiredMixin, StaffRequiredMixin, CreateView):
+class FichaMedicaCreateView(LoginRequiredMixin, EnfermeraRequiredMixin, PermissionRequiredMixin, CreateView):
     template_name = 'users/fichamedica_create.html'
     form_class = FichaMedicaForm
     success_url = reverse_lazy('users:users-fichamedica-list')
+    permission_required = 'users.add_fichamedica'
 
-class FichaMedicaUpdateView(LoginRequiredMixin, StaffRequiredMixin, UpdateView):
+    def handle_no_permission(self):
+        try:
+            return super().handle_no_permission()
+        except PermissionDenied:
+            return render(self.request, 'users/permission_denied.html', status=403)
+
+class FichaMedicaUpdateView(LoginRequiredMixin, EnfermeraRequiredMixin, PermissionRequiredMixin, UpdateView):
     template_name = 'users/fichamedica_update.html'
     form_class = FichaMedicaForm
     queryset = FichaMedica.objects.all()
     success_url = reverse_lazy('users:users-fichamedica-list')
+    permission_required = 'users.change_fichamedica'
+
+    def handle_no_permission(self):
+        try:
+            return super().handle_no_permission()
+        except PermissionDenied:
+            return render(self.request, 'users/permission_denied.html', status=403)
 
 
 
 
-class FichaMedicaDeleteView(LoginRequiredMixin, StaffRequiredMixin, DeleteView):
+
+class FichaMedicaDeleteView(LoginRequiredMixin, EnfermeraRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = FichaMedica
     success_url = reverse_lazy('users:users-fichamedica-list')
-    # Puedes especificar el template_name si deseas utilizar un template personalizado
+    permission_required = 'users.delete_fichamedica'
+
+    def handle_no_permission(self):
+        try:
+            return super().handle_no_permission()
+        except PermissionDenied:
+            return render(self.request, 'users/permission_denied.html', status=403)
 
 
-class ResidenteDetailView(LoginRequiredMixin, StaffRequiredMixin, DetailView):
-    template_name = 'users/residente_detail.html'
-    model = Residente
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        residente = self.get_object()
-        form = ResidenteDetailForm(instance=residente)
-        # Disable editing in the form
-        for field in form.fields.values():
-            field.widget.attrs['disabled'] = True
-        context['form'] = form
-        return context
+
 
 
 class FichaMedicaDetailView(LoginRequiredMixin, StaffRequiredMixin, DetailView):
     template_name = 'users/fichamedica_detail.html'
     model = FichaMedica
-
-
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
